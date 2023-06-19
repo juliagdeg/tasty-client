79117188206fd16a5ee580252320ad7e0c3b6230
Julia Degnan <juliagdegnan@gmail.com>
2023-06-18 20:35:43 -0500

    fixed application views

--- src/TastyTrails.js
+++ src/TastyTrails.js
@@ -1,39 +1,17 @@
 // import { useState } from "react"
 import { ApplicationViews } from "./views/ApplicationViews"
 import { NavBar } from "./components/nav/NavBar"
-import { Login } from "./components/auth/Login"
-import { Register } from "./components/auth/Register"
-import { Authorized } from "./views/Authorized"
-import { Route, Routes } from "react-router-dom"
-import { RecipeList } from "./components/recipes/RecipeList"
+// import { Login } from "./components/auth/Login"
+// import { Register } from "./components/auth/Register"
+// import { Authorized } from "./views/Authorized"
+// import { Route, Routes } from "react-router-dom"
+// import { RecipeList } from "./components/recipes/RecipeList"
 import "./Tasty.css"
 
 
 export const TastyTrails = () => {
-
-  const setToken = (auth_token) => {
-		let token = {
-			"token": auth_token
-		}
-		localStorage.setItem('tasty_user', JSON.stringify(token))
-	}
-
-      return <>
-        <Routes>
-            <Route path="/" element={<RecipeList /> } />
-            <Route path="/login" element={<Login setToken={setToken} />} />
-              <Route path="/register" element={<Register setToken={setToken} />} />
-              
-            <Route path="*" element={
-              <Authorized>
-                <>
-                  <NavBar />
-                  <ApplicationViews />
-                </>
-              </Authorized>
-          
-            } />
-
-        </Routes>
-    </>
+  return <>
+    <NavBar />
+    <ApplicationViews />
+  </>
 }
--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,24 +1,41 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-// import { Login } from "../components/auth/Login"
-// import { Register } from "../components/auth/Register"
-// import { Authorized } from "./Authorized"
+import { Route, Routes } from "react-router-dom"
+import { Login } from "../components/auth/Login"
+import { Register } from "../components/auth/Register"
+import { Authorized } from "./Authorized"
 import { RecipeList } from "../components/recipes/RecipeList"
 import { RecipeForm } from "../components/recipes/RecipeForm"
 import { Profile } from "../components/profile/Profile"
 
 
 export const ApplicationViews = () => {
-    return (
+    return <>
         <Routes>
-            <Route path="/" element={
-                <>
-                    <Outlet />
-                </>
-            }>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route element={<Authorized />}>
+                {/* Add Routes here */}
                 <Route path="/recipes" element={<RecipeList />} />
                 <Route path="/recipes/create" element={<RecipeForm /> } />
                 <Route path="/profile" element={<Profile /> } />
             </Route>
         </Routes>
-    )
+    </>
 }
+
+// export const ApplicationViews = () => {
+
+// }
+
+//     return (
+//     <Routes>
+//     <Route path="/" element={
+//         <>
+//             <Outlet />
+//         </>
+//     }>
+//         <Route path="/recipes" element={<RecipeList />} />
+//         <Route path="/recipes/create" element={<RecipeForm /> } />
+//         <Route path="/profile" element={<Profile /> } />
+//     </Route>
+// </Routes>
+// )
